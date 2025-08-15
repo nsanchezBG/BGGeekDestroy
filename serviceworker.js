5fe45a3b23855eef6f9f8882b0880aa9578ca1e4
nsanchezBG <nicolas@brandgeeks.pe>
2025-08-14 20:45:14 -0500

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'geek-destroy-cache-v8'; // Subimos la versión
+const CACHE_NAME = 'geek-destroy-cache-v9'; // Subimos la versión
 const urlsToCache = [
   './',
   './index.html',
